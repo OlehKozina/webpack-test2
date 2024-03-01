beb0f77bab34d5ec6478e1352fe3af10d7c34142
OlegKozina <olegkozina@icloud.com>
2024-03-01 01:17:53 -0700

    Add files via upload

---
+++ js/mobile-menu.js
@@ -0,0 +1,15 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuBtnOpen = document.querySelector('.menu-btn-open');
+const menuBtnClose = document.querySelectorAll('.menu-btn-close');
+
+const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const disableScroll = () =>
+  document.body.classList.toggle('is-scroll-disabled');
+
+menuBtnOpen.addEventListener('click', toggleMenu);
+menuBtnClose.forEach(el=>{el.addEventListener('click', toggleMenu)});
+
+menuBtnOpen.addEventListener('click', disableScroll);
+menuBtnClose.addEventListener('click', disableScroll);
+
+
